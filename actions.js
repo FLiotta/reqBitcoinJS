1a0e2028d1728078ec2a2a62130ef1961354d30c
FLiotta <liottafabrizio@gmail.com>
2019-01-07 20:39:11 -0300

    TweetNews implemented, also fixed config problem

--- actions.js
+++ actions.js
@@ -1,12 +1,15 @@
 const twit = require('twit');
 const config = require('./config');
 const axios = require('axios');
+const xml2js = require('xml2js');
+
 var T = new twit({
 	consumer_key: config.consumer_key,
 	consumer_secret: config.consumer_secret,
 	access_token: config.access_token,
 	access_token_secret: config.access_token_secret,
 });
+
 
 const tweetBTC = () => {
 	axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -19,7 +22,7 @@ const tweetBTC = () => {
 		console.log("BTC Tweeted")
 		T.post('statuses/update', { status }, function(err, data, response) {
 			if(err)
-				console.log("err");
+				console.log(err);
 		})
 	})
 	.catch(e => console.log(e));	
@@ -33,11 +36,11 @@ const tweetCryptos = () => {
 		.then((res) => {
 			const status = `
 ${new Date().toISOString().replace("T", ' ').replace(/\..+/, '')} \n
-#Etherum: $${res.data.ETH.USD} | €${res.data.ETH.EUR} \n
-#Litecoin: $${res.data.LTC.USD} | €${res.data.LTC.EUR} \n
-#Ripple: $${res.data.XRP.USD} | €${res.data.XRP.EUR} \n
-#BitcoinCash: ${res.data.BCH.USD} | €${res.data.BCH.EUR} \n
-#Stellar: $${res.data.XLM.USD} | €${res.data.XLM.EUR} \n
+#Etherum: $${res.data.ETH.USD} | €${res.data.ETH.EUR} 
+#Litecoin: $${res.data.LTC.USD} | €${res.data.LTC.EUR}
+#Ripple: $${res.data.XRP.USD} | €${res.data.XRP.EUR} 
+#BitcoinCash: ${res.data.BCH.USD} | €${res.data.BCH.EUR}
+#Stellar: $${res.data.XLM.USD} | €${res.data.XLM.EUR} 
 
 #Cryptocurrencies prices provided by @CryptoCompare 🥴`
 			console.log("Other cryptos tweeted");
@@ -48,4 +51,29 @@ ${new Date().toISOString().replace("T", ' ').replace(/\..+/, '')} \n
 		})
 }
 
-module.exports = { tweet_price, tweet_otherCryptos };
+const tweetNew = () => {
+	axios.get('https://www.ccn.com/news/feed/')
+			.then((res) => {
+				xml2js.parseString(res.data, (err, data) => {
+					
+					let status = `
+${data.rss.channel[0].item[0].title}\n
+
+${data.rss.channel[0].item[0].link}\n
+
+`
+					data.rss.channel[0].item[0].category.forEach((tag) => {
+						let hashtag = "#" + tag.replace(/ /g, "");
+						status = status + " " + hashtag;
+					})
+
+					T.post('statuses/update', { status }, function(err, data, response) {
+						if(err)
+							console.log(err);
+					})
+
+				})
+			})
+}
+
+module.exports = { tweetBTC , tweetCryptos, tweetNew };
--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const actions = require('./actions');
 
-setInterval(() => actions.tweetBTC(), 1000 * 60 * 30);
+
+setInterval(() => {
+	actions.tweetBTC();
+
+	setTimeout(() => actions.tweetNew(), 1000 * 10);
+	
+}, 1000 * 60 * 30);
 
 setInterval(() => actions.tweetCryptos(), 1000 * 60 * 60);
-
-
-
-
-
