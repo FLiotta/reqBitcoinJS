0e15089078512ac60b5dff5de87de03dde33ed6c
FLiotta <liottafabrizio@gmail.com>
2019-01-07 18:36:26 -0300

    Added tweet_otherCryptos method

--- actions.js
+++ actions.js
@@ -1,7 +1,12 @@
 const twit = require('twit');
 const config = require('./config');
 const axios = require('axios');
-var T = new twit(config);
+var T = new twit({
+	consumer_key: config.consumer_key,
+	consumer_secret: config.consumer_secret,
+	access_token: config.access_token,
+	access_token_secret: config.access_token_secret,
+});
 
 const tweet_price = () => {
 	axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -9,7 +14,7 @@ const tweet_price = () => {
 		const status = `
 			${res.data.time.updated} \n\n$${res.data.bpi.USD.rate.split('.')[0]} USD \n€${res.data.bpi.EUR.rate.split('.')[0]} EUR \n£${res.data.bpi.GBP.rate.split('.')[0]} GBP
 			\n
-			#Bitcoin #BTC
+			#Bitcoin #BTC 🥴
 			`;
 		console.log(status)
 		T.post('statuses/update', { status }, function(err, data, response) {
@@ -20,4 +25,23 @@ const tweet_price = () => {
 	.catch(e => console.log(e));	
 }
 
-module.exports = { tweet_price };
+const tweet_otherCryptos = () => {
+	axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=LTC,ETH,XLM,XRP,BCH&tsyms=USD,EUR', { 
+		headers: {
+			apiKey: config.api_key
+		}})
+		.then((res) => {
+			const status = `
+${new Date().toISOString().replace("T", ' ').replace(/\..+/, '')} \n
+#Etherum: $${res.data.ETH.USD} | €${res.data.ETH.EUR} \n
+#Litecoin: $${res.data.LTC.USD} | €${res.data.LTC.EUR} \n
+#Ripple: $${res.data.XRP.USD} | €${res.data.XRP.EUR} \n
+#BitcoinCash: ${res.data.BCH.USD} | €${res.data.BCH.EUR} \n
+#Stellar: $${res.data.XLM.USD} | €${res.data.XLM.EUR} \n
+
+#Cryptocurrencies prices provided by @CryptoCompare 🥴`
+			console.log(status);
+		})
+}
+
+module.exports = { tweet_price, tweet_otherCryptos };
--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
 const actions = require('./actions');
 
-setInterval(() => {
-	actions.tweet_price();
-}, 1000 * 60 * 60)
+setInterval(() => actions.tweet_price(), 1000 * 60 * 30);
+
+setInterval(() => actions.tweet_otherCryptos(), 1000 * 60 * 60);
+
+
+
+
+
