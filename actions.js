c0d0b8c2fc3837e5c10ef38497a1e7cc07ab950f
FLiotta <liottafabrizio@gmail.com>
2019-01-06 21:46:16 -0300

    Changed tweet message

--- actions.js
+++ actions.js
@@ -6,7 +6,11 @@ var T = new twit(config);
 const tweet_price = () => {
 	axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 	.then((res) => {
-		const status = `[${res.data.time.updated}] BTC Price: $${res.data.bpi.USD.rate.split('.')[0]} usd.`;
+		const status = `
+			${res.data.time.updated} \n\n$${res.data.bpi.USD.rate.split('.')[0]} USD \n€${res.data.bpi.EUR.rate.split('.')[0]} EUR \n£${res.data.bpi.GBP.rate.split('.')[0]} GBP
+			\n
+			#Bitcoin #BTC
+			`;
 		console.log(status)
 		T.post('statuses/update', { status }, function(err, data, response) {
 			if(err)
